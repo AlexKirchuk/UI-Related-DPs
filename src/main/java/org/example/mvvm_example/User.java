822cd0df3f4d752efdb92c411037b1611028374a
Anastasia Korytko <nastya19korytko@gmail.com>
2025-11-05 21:17:34 +0300

    commit1

---
+++ src/main/java/org/example/mvvm_example/User.java
@@ -0,0 +1,10 @@
+package org.example.mvvm_example;
+
+public class User {
+    private String name;
+
+    public User(String name) { this.name = name; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
